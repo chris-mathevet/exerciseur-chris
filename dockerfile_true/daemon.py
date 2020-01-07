ff63fd2504c56d9c0907a543d97ac52d96c52f10
Florent Becker <Florent.Becker@ens-lyon.org>
2020-01-07 17:56:59 +0100

    Correction du dockerfile trivial

--- dockerfile_true/daemon.py
+++ dockerfile_true/daemon.py
@@ -6,24 +6,17 @@ from socketserver import StreamRequestHandler, TCPServer
 """
 Un serveur pour répondre à une tentative étudiante
 On est content quoi qu'on nous propose…
-
 """
-
-# def serve(port):
-#     sock = 
 
 class ToujoursContent(StreamRequestHandler):
 
     def handle(self):
         code_etu = cbor.load(self.rfile)
-        print(code_etu)
         self.wfile.write(json.dumps({"résultat": "fantastique"}).encode())
-        self.wfile.close()
-        
+        self.wfile.flush()
+
 if __name__ == "__main__":
     port = 5678 if len(sys.argv) < 2 else int(sys.argv[1])
+    with TCPServer(("", port), ToujoursContent) as serv:
+       serv.serve_forever()
 
-    with TCPServer(("localhost", port), ToujoursContent) as serv:
-        serv.serve_forever()
-            
-                         
