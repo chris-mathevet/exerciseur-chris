91af43ff03953029cb48ae0f671f93d022a24b4e
Florent Becker <Florent.Becker@ens-lyon.org>
2020-03-04 14:50:15 +0100

    Séparation de StreamTee dans son propre module

--- docker_exerciseur/exerciseur.py
+++ docker_exerciseur/exerciseur.py
@@ -14,17 +14,7 @@ from types import ModuleType
 import cbor
 
 from . import jacadi
-
-
-class StreamTee:
-    def __init__(self, out1, out2):
-        self.out1 = out1
-        self.out2 = out2
-
-    def write(self, *args, **kwargs):
-        self.out1.write(*args, **kwargs)
-        self.out2.write(*args, **kwargs)
-
+from .stream_tee import StreamTee
 
 class PaquetExercice:
     def __init__(self, contenu: bytes, type_exo, métadonnées):
---
+++ docker_exerciseur/stream_tee.py
@@ -0,0 +1,8 @@
+class StreamTee:
+    def __init__(self, out1, out2):
+        self.out1 = out1
+        self.out2 = out2
+
+    def write(self, *args, **kwargs):
+        self.out1.write(*args, **kwargs)
+        self.out2.write(*args, **kwargs)
