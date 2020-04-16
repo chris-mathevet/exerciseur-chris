f6aa9e9cfc6156d17bf0ac24bd2f4bc9d81251ac
Florent Becker <Florent.Becker@posteo.net>
2020-04-16 15:57:17 +0200

    docker-exerciseur construit --prépare
    Option qui permet de préparer les souces dans un répertoire temporaire
    sans créer d'image docker.

--- docker_exerciseur/constructeur.py
+++ docker_exerciseur/constructeur.py
@@ -21,6 +21,10 @@ parser.add_argument(
 parser.add_argument(
     "--module", help="le module de tests de l'exerciseur"
 )
+parser.add_argument(
+    "--prépare", help="ne construit pas l'image docker, mais crée seulement le répertoire avec un Dockerfile pour docker build",
+    action="store_true"
+)
         
 def main(args):
     dossier_source = args.dossier or "."
@@ -32,8 +36,12 @@ def main(args):
             métadonnées['fichier_ens'] = args.module
         else:
             métadonnées['nom_module'] = args.module
-    id_image = construit_exerciseur(args.type, dossier_source, args.verbose, **métadonnées)
-    print(id_image)
+    if args.prépare:
+        chemin = prépare_exerciseur(args.type, dossier_source, args.verbose, **métadonnées)
+        print(chemin)
+    else:
+        id_image = construit_exerciseur(args.type, dossier_source, args.verbose, **métadonnées)
+        print(id_image)
 
 
 def construit_exerciseur(type_ex, dossier_source, verbose, **kwargs):
@@ -54,3 +62,27 @@ def construit_exerciseur(type_ex, dossier_source, verbose, **kwargs):
     dossier_source = os.path.abspath(dossier_source)
     ex = Exerciseur.avec_type(dossier_source, type_ex, debug_out=debug_out, **kwargs)
     return  ex.construire()
+
+def prépare_exerciseur(type_ex, dossier_source, verbose, **kwargs):
+    """
+    Construit un exerciseur. Les arguments correspondent à ceux de `docker-exerciseur construit`
+
+    @param type_ex: le type d'exerciseur, parmi "DémonPy", "PackagePy", "TestsPy", "Dockerfile" ou "Jacadi"
+    @param dossier_source: le chemin des sources de l'exerciseur
+    @param verbose: un booléen, vrai pour afficher plus d'informations sur sys.stderr
+    @param kwarg: un dictionnaire qui sert à donner des arguments supplémentaires en fonction de `type_ex`.
+    - pour PackagePy, `nom_module="tralala"` indique quel module contient la classe exerciseur et `nom_classe="NomClasse"` le nom de cette classe
+    - pour TestsPython, `nom_module="tralala"` indique quel module contient les tests
+    - pour Jacadi, `module="mod_ens"` indique quel module contient le code enseignant.
+
+    @return l'idententifiant de l'image construite pour cet exerciseur.
+    """
+    debug_out = verbose and sys.stderr
+    dossier_source = os.path.abspath(dossier_source)
+    ex = Exerciseur.avec_type(dossier_source, type_ex, debug_out=debug_out, **kwargs)
+    import tempfile
+    td = tempfile.mkdtemp()
+    ex.utiliser_rép_travail(td + '/src')
+    ex.copie_source()
+    ex.prépare_source()
+    return td
