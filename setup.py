431b254f11ba6c49df42aed974354190a714ec5d
chris-mathevet <chris.mathevet@etu.univ-orleans.fr>
2025-04-11 08:53:47 +0200

    version 0.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ entrées = {
 
 setup(
     name="docker-exerciseur",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     author='Florent Becker <florent.becker@univ-orleans.fr>',
     install_requires=dépendances,
